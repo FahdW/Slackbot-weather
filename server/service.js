07db59ac0b78e1ba6cc38719e62d6b0eccebd30e
Fahd Wahab <fahd.jwahab@gmail.com>
2017-05-10 20:38:45 -0400

    init commit

--- server/service.js
+++ server/service.js
@@ -2,5 +2,20 @@
 
 const express = require('express');
 const service = express();
+const request = require('superagent');
+const moment = require('moment');
+
+service.get('/service/:location', (req, res, next) => {
+  request.get('http://api.openweathermap.org/data/2.5/weather?q=' + req.params.location + `&appid=${process.env.WEATHER_KEY}&units=metric`, (err, response) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500)
+    }
+
+    const weather = response.body.main.temp;
+    
+    res.json({result: weather});
+    });
+  });
 
 module.exports = service;
